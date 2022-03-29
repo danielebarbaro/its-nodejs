c4347c6e2896d1d089fdf571703aec447976f55c
Daniele Barbaro <barbaro.daniele@gmail.com>
2022-03-29 14:32:55 +0200

    Corretto Typo e revisionato instogramma

--- exercises/lessons/1/solutions/instogramma.js
+++ exercises/lessons/1/solutions/instogramma.js
@@ -6,15 +6,18 @@
  *  ###
  **/
 
+const range = [2, 4, 5, 3];
+
+console.log('Range: ', range);
 
 let instogramma = function (range) {
     for (let value of range) {
         let result = '';
-        for (let hashtag = 0 ; hashtag < value ;value--) {
+        for (let hashtag = 0; hashtag < value; value--) {
             result += '#';
         }
         console.log(result);
     }
 }
 
-instogramma([2, 4, 5, 3]);
+instogramma(range);
