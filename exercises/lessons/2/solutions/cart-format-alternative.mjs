dd075dc56da562cb8925ed4d693c9e0914e4a296
Daniele Barbaro <barbaro.daniele@gmail.com>
2022-03-29 17:06:58 +0200

    soluzion di cart-format

---
+++ exercises/lessons/2/solutions/cart-format-alternative.mjs
@@ -0,0 +1,45 @@
+import {products} from "./dataset.mjs";
+
+let cart = [];
+console.log('Products', products);
+
+/*
+Restituire un elenco di prodotti ordinato con
+i nomi prodotti in modo che tutti i product.name abbiano la prima iniziale maiuscola
+
+ [
+    {name: 'Firenze', ean: '812302', price: 9.99, type: 'pocket-book'},
+    {name: 'Firenze', ean: '1222222', price: 19.99, type: 'book'},
+    {name: 'Friuli Venezia Giulia', ean: '12345', price: 12.10, type: 'book'},
+    {name: 'Mediterraneo', ean: '0120139', price: 22.10, type: 'photo-book'},
+    {name: 'Piemonte', ean: '29834', price: 12.10, type: 'book'},
+    {name: 'Roma', ean: '912302', price: 9.99, type: 'pocket-book'},
+];
+*/
+
+const format = (product) => {
+    return product.toLowerCase().charAt(0).toUpperCase() + product.toLowerCase().slice(1)
+}
+
+for (let item of products) {
+    let formattedName = '';
+    let productName = item.name
+
+    let multiName = productName.split(' ');
+
+    if (multiName.length > 1) {
+        let res = [];
+        for (let piece of multiName) {
+            res.push(format(piece));
+        }
+        formattedName = res.join(' ')
+    } else {
+        formattedName = format(productName);
+    }
+
+    item.name = formattedName;
+
+    cart.push(item);
+}
+
+console.log('Cart: ', cart);
