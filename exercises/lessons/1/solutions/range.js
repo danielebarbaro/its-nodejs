f185405c5b48b59795a7fb81a569f0f881cb24e0
Daniele Barbaro <barbaro.daniele@gmail.com>
2022-04-11 08:32:34 +0200

    Revisione con commenti della soluzione Range della prima lezione

--- exercises/lessons/1/solutions/range.js
+++ exercises/lessons/1/solutions/range.js
@@ -1,11 +1,35 @@
-/** Stampare il range `min` and `max` */
+/**
+ *
+ * Stampare il range di numeri compresi tra `min` e `max`
+ * Es:
+ *      const result = range(3,7)
+ *      console.log('Il range tra 3 e 7 è:', result);
+ *
+ * */
 
-let range = function (min, max) {
+// Per facilità di esecuzione di più esempi dichiaro staticamente gli espremi del range da sommare.
+const fromNumber = 3;
+const toNumber = 7;
+
+// Soluzione con funzione dichiarativa - function declaration
+
+function createRange(fromNumber, toNumber) {
     let result = [];
-    for(let index = min; index <= max; index++){
+    for (let index = fromNumber; index <= toNumber; index++) {
         result.push(index);
     }
     return result;
 }
 
-console.log('Range: ', range(4, 9), '\n');
+console.log(`Il range tra ${fromNumber} e ${toNumber} con createRange() è:`, createRange(fromNumber, toNumber), '\n');
+
+// Soluzione con funzione anonima - Anonymous function
+let range = function (fromNumber, toNumber) {
+    let result = [];
+    for (let index = fromNumber; index <= toNumber; index++) {
+        result.push(index);
+    }
+    return result;
+}
+
+console.log(`Il range tra ${fromNumber} e ${toNumber} con range() è:`, createRange(fromNumber, toNumber), '\n');
