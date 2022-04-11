7b7422f5e994e67ef142c701f54b991ce9d400b0
Daniele Barbaro <barbaro.daniele@gmail.com>
2022-04-11 08:22:27 +0200

    Revisione con commenti della soluzione Somma della prima lezione

--- exercises/lessons/1/solutions/sum.js
+++ exercises/lessons/1/solutions/sum.js
@@ -1,24 +1,68 @@
-/** Sommare i numeri tra il range `min` and `max` compresi gli estremi */
+/**
+ *
+ * Sommare i numeri compresi tra il range `min` e `max` compresi gli estremi
+ * Es:
+ *      const result = sum(3,7)
+ *      console.log('La somma del range è:', result);
+ *
+ *
+ * */
 
-let range = function (min, max) {
-    let result = [];
+// Per facilità di esecuzione di più esempi dichiaro staticamente gli espremi del range da sommare.
+const fromNumber = 3;
+const toNumber = 7;
+
+// Soluzione con funzione dichiarativa - function declaration
+function calculateSum(min, max) {
+    let result = 0;
     for (let index = min; index <= max; index++) {
+        result += index;
+    }
+    return result;
+}
+
+console.log('Risultato di calculateSum: ', calculateSum(fromNumber, toNumber), '\n');
+
+// Soluzione con funzione anonima - Anonymous function
+let calculateAnonymousSum = function (min, max) {
+    let result = 0;
+    for (let index = min; index <= max; index++) {
+        result += index;
+    }
+    return result;
+}
+console.log('Risultato di calculateAnonymousSum: ', calculateAnonymousSum(fromNumber, toNumber), '\n');
+
+// Soluzione alternativa con la costruzione di un range
+
+let createRange = function (from, to) {
+    let result = [];
+    for (let index = from; index <= to; index++) {
         result.push(index);
     }
     return result;
 }
 
-let sum = function (range) {
+// Che può anche esser scritta cosi:
+/*
+ * function createRange(from, to) {
+ *   let result = [];
+ *   for (let index = from; index <= to; index++) {
+ *      result.push(index);
+ *   }
+ *   return result;
+ * }
+* */
+
+let sumRangeValues = function (range) {
     let result = 0;
     for (let number of range) {
-        // prova a sostiture of con in
-        // console.log('number type', typeof number);
+        // prova a sostiture of con in, Cosa succede?!
         result += number;
     }
     return result;
 }
 
-const res = range(1, 3)
+const rangeValues = createRange(fromNumber, toNumber)
 
-console.log('Range: ', res, '\n');
-console.log('Sum range: ', sum(res), '\n');
+console.log('Risultato di calculateAnonymousSum: ', sumRangeValues(rangeValues));
